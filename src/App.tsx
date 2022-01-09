769282f889ae3314f37868490d1983832e065365
monika-pant <monikapant.1feb@gmail.com>
2022-01-10 00:21:24 +0530

    convert react app to use type script

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import Todo from "./components/Todo";
+import React from 'react';
+import Header from './components/Header'
+function App() {
+  return (
+    <div>
+      <Header title="Hello World" color="red"></Header>
+      {/* <h1> My Todos</h1>
+      <Todo title='learn React day 1'/>
+      <Todo title='learn React day 2'/>
+      <Todo title='learn React day 3'/>
+      <Todo title='learn React day 4'/> */}
+    </div>
+  );
+}
+
+export default App;
