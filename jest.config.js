d648821a973fa0bee116b21905ad2d0e25654494
Manu Kapoor <mkapoor@tillster.com>
2022-01-10 19:25:47 +0530

    Add first working test

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,6 @@ module.exports = {
     // when using React Testing Library and adds special
     // extended assertions to Jest
     setupFilesAfterEnv: [
-      "@testing-library/react/cleanup-after-each",
       "@testing-library/jest-dom/extend-expect"
     ],
   
@@ -23,5 +22,6 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   
     // Module file extensions for importing
+    testEnvironment: 'jsdom',
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
   };
